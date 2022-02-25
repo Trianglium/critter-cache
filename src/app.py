969faed9f286d246c43f9305f7c3d7271f5c902b
Trianglium <jbrockerrudow@gmail.com>
2022-02-25 15:04:43 -0600

    Main Menu Integrated

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 import pygame
 import config as gc
-
 from pygame import display, event, image
 from time import sleep
 from animal import Animal
+import os
+
+import pygame_menu
 
 def find_index(x, y):
     row = y // gc.IMAGE_SIZE
@@ -12,68 +14,80 @@ def find_index(x, y):
     return index
 
 pygame.init()
-
 display.set_caption("Critter Cache")
-screen = display.set_mode((512, 512))
-matched = image.load('other_assets/matched.png')
+surface = display.set_mode((512, 512))
 
+def start_the_game():
+    display.set_caption("Critter Cache")
+    screen = display.set_mode((512, 512))
+    matched = image.load('other_assets/matched.png')
+    running = True
+    tiles = [Animal(i) for i in range(0, gc.NUM_TILES_TOTAL)]
+    current_images = []
+    score = 0
 
-running = True
-tiles = [Animal(i) for i in range(0, gc.NUM_TILES_TOTAL)]
-current_images = []
-score = 0
+    while running:
+        current_events = event.get()
 
-while running:
-    current_events = event.get()
+        for e in current_events:
+            if e.type == pygame.QUIT:
+                running = False
 
-    for e in current_events:
-        if e.type == pygame.QUIT:
-            running = False
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE:
+                running = False
 
-    if e.type == pygame.KEYDOWN:
-        if e.key == pygame.K_ESCAPE:
-            running = False
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            index = find_index(mouse_x, mouse_y)
+            if index not in current_images:
+                current_images.append(index)
+            if len(current_images) > 2:
+                current_images = []
 
-    if e.type == pygame.MOUSEBUTTONDOWN:
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        index = find_index(mouse_x, mouse_y)
-        if index not in current_images:
-            current_images.append(index)
-        if len(current_images) > 2:
-            current_images = []
+        screen.fill(gc.BG_DARK)
+        total_skipped = 0
 
-    screen.fill(gc.BG_DARK)
-    total_skipped = 0
+        for _, tile in enumerate(tiles):
+            image_i = tile.image if tile.index in current_images else tile.box
+            if not tile.skip:
+                screen.blit(image_i, (tile.col * gc.IMAGE_SIZE + gc.MARGIN, tile.row * gc.IMAGE_SIZE + gc.MARGIN))
+            else:
+                total_skipped += 1
 
-    for _, tile in enumerate(tiles):
-        image_i = tile.image if tile.index in current_images else tile.box
-        if not tile.skip:
-            screen.blit(image_i, (tile.col * gc.IMAGE_SIZE + gc.MARGIN, tile.row * gc.IMAGE_SIZE + gc.MARGIN))
-        else:
-            total_skipped += 1
-
-    display.flip()
-
-    if len(current_images) == 2:
-        idx1, idx2 = current_images
-        if tiles[idx1].name == tiles[idx2].name:
-            tiles[idx1].skip = True
-            tiles[idx2].skip = True
-            sleep(0.4)
-            screen.blit(matched, (0, 0))
-            display.flip()
-            sleep(0.4)
-            current_images = []
-
-        elif tiles[idx1].name != tiles[idx2].name:
-            sleep(0.4)
-            display.flip()
-            current_images = []
-            print("Opps! Not a match!")
         display.flip()
 
-    if total_skipped == len(tiles):
-        running = False
+        if len(current_images) == 2:
+            idx1, idx2 = current_images
+            if tiles[idx1].name == tiles[idx2].name:
+                tiles[idx1].skip = True
+                tiles[idx2].skip = True
+                sleep(0.4)
+                screen.blit(matched, (0, 0))
+                display.flip()
+                sleep(0.4)
+                current_images = []
+
+            elif tiles[idx1].name != tiles[idx2].name:
+                sleep(0.4)
+                display.flip()
+                current_images = []
+                print("Opps! Not a match!")
+            display.flip()
+
+        if total_skipped == len(tiles):
+            running = False
 
 
-print("Goodbye")
+    print("Goodbye")
+
+
+
+
+menu = pygame_menu.Menu('Critter Cache', 400, 300,
+                       theme=pygame_menu.themes.THEME_SOLARIZED)
+
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+menu.mainloop(surface)
